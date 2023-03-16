7a9495d3bd4dfe439d40d6956bcc668711aa6f87
amir-91 <61394176+amir-91@users.noreply.github.com>
2023-03-17 01:06:44 +0200

    fix: handle cards size

--- src/components/GenericComponents/ErrorPage/ErrorPage.vue
+++ src/components/GenericComponents/ErrorPage/ErrorPage.vue
@@ -2,5 +2,6 @@
   <div class="error-page">
     <div><img src="../../../assets/images/something-went-wrong.svg" /></div>
     <div><p>Something went wrong, please try again later</p></div>
+    <a href="/">Go to homepage</a>
   </div>
 </template>
