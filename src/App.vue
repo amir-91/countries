b1ef508f329204049e9907d4321db6b5a3fa843c
amir-91 <61394176+amir-91@users.noreply.github.com>
2023-03-27 17:27:01 +0200

    feat: create back to top btn

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <Header></Header>
   <router-view></router-view>
+  <Btn></Btn>
 </template>
 
 <script>
 import Header from "./components/Layout/HeaderCompnent.vue";
+import Btn from "./components/BackToTopBtn/BackToTopBtn.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Btn
   },
 };
 </script>
---
+++ src/components/BackToTopBtn/BackToTopBtn.vue
@@ -0,0 +1,41 @@
+<template>
+    <button
+        ref="backToTop"
+        v-if="showBtn"
+        type="button"
+        class="btn"
+        id="btn-back-to-top"
+        @click="backToTop"
+        >
+        <ion-icon name="chevron-up-outline"></ion-icon>
+</button>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            showBtn: false
+        }
+    },
+    methods: {
+        scrollFunction() {
+            if (
+                document.body.scrollTop > 200 ||
+                document.documentElement.scrollTop > 200
+            ) {
+                this.showBtn = true
+            } else {
+                this.showBtn = false
+            }
+        },
+        backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    },
+    mounted(){
+        window.onscroll = () => this.scrollFunction();
+    }
+}
+</script>
